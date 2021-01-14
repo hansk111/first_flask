cf1edf71818b243045b992f024e8d35996fde4d7
hanseokhee <hanseokhee.han@gmail.com>
2021-01-14 20:45:32 +0900

    게시판 링크 수정

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -4,11 +4,7 @@ from werkzeug.utils import redirect
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-@bp.route('/hello')
-def hello_pybo():
-    return 'Hello, Pybo!'
-
 
 @bp.route('/')
 def index():
-    return redirect(url_for('question._list'))
+    return redirect(url_for('mainscreen.mainscreen'))
